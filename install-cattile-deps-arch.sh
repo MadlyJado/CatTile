43bc4fb33aa33ff70aea7ceee637d522952f2be3
MadlyJado <madlyjado@proton.me>
2024-01-29 17:19:53 -0500

    Added some text to show what the install-cattile-deps-arch.sh script is doing

--- install-cattile-deps-arch.sh
+++ install-cattile-deps-arch.sh
@@ -1,6 +1,9 @@
+echo "Installing non-aur dependencies first!"
 pacman -Syu qtile python-iwlib python-psutil python-dbus python-dbus-next git base-devel picom nitrogen polkit-gnome rofi --needed
+echo "Installing the paru aur helper to install aur dependencies"
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
+echo "Installing aur dependencies"
 paru -S ttf-hack-nerd-font ttf-weather-icons qtile-extras
 echo "CatTile Bar dependencies are now fully installed!"
